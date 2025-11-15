4685b7920ed761ae9aae26cc6aca097dad35e24c
Alfaepsilon <36788922+Alfaepsilon@users.noreply.github.com>
2025-11-15 23:15:17 +0100

    Create config.cmd

---
+++ configuration/config.cmd
@@ -0,0 +1,12 @@
+@echo off
+@REM To install software onto the system during setup, winget is used as it is native to Windows. The software to be installed can be configured by editing the "requirements.txt" file
+winget source reset --force
+winget source update
+for /f %%p in (software.txt) do (
+    winget install --id=%%p -e --source winget --silent --accept-package-agreements --accept-source-agreements
+)
+
+@REM To install Visual Studio Code extensions during setup, the "code" executable is used
+for /f %%p in (vscode_extensions.txt) do (
+    code --install-extension %%p
+)

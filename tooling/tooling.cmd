7622829a5f5be1065bdfb2d9ec866dda7f375a27
Alfaepsilon <36788922+Alfaepsilon@users.noreply.github.com>
2025-11-16 14:18:41 +0100

    Create tooling.cmd

---
+++ tooling/tooling.cmd
@@ -0,0 +1,11 @@
+@echo off
+setlocal
+@REM To install software onto the system during setup, winget is used as it is native to Windows. The software to be installed can be configured by editing the "requirements.txt" file
+for /f %%p in (software.txt) do (
+    winget install --id=%%p -e --source winget --silent --accept-package-agreements --accept-source-agreements
+)
+
+@REM To install Visual Studio Code extensions during setup, the "code" executable is used. The extensions can be configured by editing the "vscode_extensions.txt" file
+for /f %%p in (vscode_extensions.txt) do (
+    code --install-extension %%p
+)

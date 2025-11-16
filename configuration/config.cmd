227901b824ac3714e4db7aadea46b0f3152aa9ee
Alfaepsilon <36788922+Alfaepsilon@users.noreply.github.com>
2025-11-16 14:22:29 +0100

    Update config.cmd

--- configuration/config.cmd
+++ configuration/config.cmd
@@ -1,10 +1,4 @@
 @echo off
-@REM To install software onto the system during setup, winget is used as it is native to Windows. The software to be installed can be configured by editing the "requirements.txt" file
-for /f %%p in (software.txt) do (
-    winget install --id=%%p -e --source winget --silent --accept-package-agreements --accept-source-agreements
-)
-
-@REM To install Visual Studio Code extensions during setup, the "code" executable is used
-for /f %%p in (vscode_extensions.txt) do (
-    code --install-extension %%p
-)
+setlocal
+@REM Add exclusion path for pentesting tools.
+powershell.exe -c "Add-MpPreference -ExclusionPath 'C:\Users\Administrator\Tools'"

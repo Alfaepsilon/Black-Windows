cbcef4afaead80ae2d27cf55760f19ec4ab97958
Alfaepsilon <36788922+Alfaepsilon@users.noreply.github.com>
2025-11-17 21:22:43 +0100

    Update BlackWindows.cmd

--- BlackWindows.cmd
+++ BlackWindows.cmd
@@ -1,6 +1,13 @@
 @echo off
 SETLOCAL EnableDelayedExpansion
 
+net session >nul 2>&1
+if %errorlevel% neq 0 (
+    echo Requesting administrative privileges...
+    powershell -Command "Start-Process -FilePath '%~f0' -Verb RunAs"
+    exit /b
+)
+
 goto :banner
 
 :main
